ed09cd053c4171d7a420674fdd28cf3395c10dca
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-09-21 19:48:45 +0200

    set config

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
         // baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        screenshot: "only-on-failure",
         trace: "on-first-retry",
     },
 
@@ -53,12 +54,12 @@ export default defineConfig({
             name: "firefox",
             use: { ...devices["Desktop Firefox"] },
         },
-
+        /*
         {
             name: "webkit",
             use: { ...devices["Desktop Safari"] },
         },
-
+        */
         /* Test against mobile viewports. */
         // {
         //   name: 'Mobile Chrome',
