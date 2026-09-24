304e73081758eafe75b0bc9984376c94decdef05
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-09-24 11:06:53 +0200

    fixed some bugs in AddNewEmployeeTest

--- tests/PIM/AddNewEmployeeTest.spec.ts
+++ tests/PIM/AddNewEmployeeTest.spec.ts
@@ -17,35 +17,43 @@ test.describe("TC04 Add new employee and delete test", () => {
         test.skip(data.run !== "yes", "Run flag is not yes.");
 
         var employee: NewEmployee = {
-            firstName: await pimPage.generateRandomCharacters(8),
-            middleName: await pimPage.generateRandomCharacters(8),
-            lastName: await pimPage.generateRandomCharacters(8),
-            employeeId: "",
+            firstName: await pimPage.generateRandomCharacters(8, false),
+            middleName: await pimPage.generateRandomCharacters(8, false),
+            lastName: await pimPage.generateRandomCharacters(8, false),
+            employeeId: await pimPage.generateRandomCharacters(8, true),
         };
 
         await test.step("Login", async () => {
             await loginPage.gotoLoginPage();
             await loginPage.login(data.username, data.password);
         });
-
         await test.step("Add New Employee", async () => {
             await dashBoardPage.pimButton.click();
             await pimPage.addNewEmployeeButton.click();
 
-            await pimPage.fillTheFormAndSave(
-                employee.firstName,
-                employee.middleName,
-                employee.lastName,
-            );
-            employee.employeeId = await pimPage.getEmployeeId();
+            await pimPage.fillTheFormAndSave(employee);
+
+            console.log("employeeId: " + employee.employeeId);
+            console.log("lastName:" + employee.lastName);
         });
 
         await test.step("Validate new employee is in the list", async () => {
-            await pimPage.employeeListButton.click();
-            await pimPage.employeeNameSearchInput.fill(employee.lastName);
             // Megvárjuk, amíg az oldal feldolgozza a kérést.
             await page.waitForLoadState("networkidle");
+            await pimPage.employeeListButton.click();
+            //  await pimPage.employeeNameSearchInput.fill(employee.lastName);
+            await pimPage.employeeIdInput.fill(employee.employeeId);
             await pimPage.searchEmployeeButton.click();
+
+            // Megvárjuk, amíg az oldal feldolgozza a kérést.
+            await page.waitForLoadState("networkidle");
+
+            var resultText: string = await pimPage.getResultLabelText();
+            console.log(resultText);
+
+            if (resultText !== "(1) Records Found") {
+                await pimPage.searchEmployeeButton.click();
+            }
 
             (await pimPage.getSpecificEmployeeIdByLastName(
                 employee.lastName,
